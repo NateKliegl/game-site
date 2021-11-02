9e78eb8833a274dce5f4d017252f94d7dfccc838
Nate Kliegl <nkliegl2@gmail.com>
2021-11-02 08:33:45 -0500

    gets the user backend set up

---
+++ server/models/user.models.js
@@ -0,0 +1,76 @@
+const query = require("../config/mysql.conf");
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
+const jwt = require("jsonwebtoken");
+
+async function signup(res, username, password) {
+  try {
+    const [user] = await query("SELECT * FROM users WHERE users.username = ?", [
+      username,
+    ]);
+    if (user) {
+      return res.send({
+        success: false,
+        data: null,
+        error: "Username Already Taken",
+      });
+    }
+    const hash = await bcrypt.hash(password, 10);
+    const uuid = uuidv4();
+    await query(
+      "INSERT INTO users (username, password, uuid) VALUES (?, ?, ?)",
+      [username, password, uuid]
+    );
+    return res.send({
+      success: true,
+      error: null,
+      data: "Signed Up!",
+    });
+  } catch (e) {
+    return res.send({
+      success: false,
+      data: null,
+      error: "Something went wrong, please try again later!",
+    });
+  }
+}
+
+async function login(res, username, password) {
+  try {
+    const [user] = await query("SELECT * FROM users WHERE users.username = ?", [
+      username,
+    ]);
+    if (!user) {
+      return res.send({
+        success: false,
+        data: null,
+        error: "Wrong Username",
+      });
+    }
+    let match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.send({
+        success: false,
+        data: null,
+        error: "Wrong Password",
+      });
+    }
+    const payload = { uuid: user.uuid };
+    const token = jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: "7 days",
+    });
+    return res.cookie("jwt", token, { httpOnly: true, maxAge: 36000 }).send({
+      success: true,
+      data: { username: user.username },
+      error: null,
+    });
+  } catch (e) {
+    return res.send({
+      success: false,
+      error: "Something went wrong, please try again later!",
+      data: null,
+    });
+  }
+}
+
+module.exports = { signup, login };
---
+++ server/routes/user.routes.js
@@ -0,0 +1,58 @@
+const express = require("express");
+
+const router = express.Router();
+const auth = require("../middleware/auth.middleware");
+const { login, signup } = require("../models/user.models");
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("jwt");
+  return res.send({
+    success: true,
+    data: "Signed out",
+    error: null,
+  });
+});
+
+router.get("/verify", auth, (req, res) => {
+  return res.send({
+    success: true,
+    error: null,
+    data: { username: req.user.username },
+  });
+});
+
+router.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+  if (validate(username, password)) {
+    return res.send({
+      success: false,
+      error: "Invalid information",
+      data: null,
+    });
+  }
+  signup(res, username, password);
+});
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (validate(username, password)) {
+    return res.send({
+      success: false,
+      data: null,
+      error: "Invalid information",
+    });
+  }
+});
+
+function validate(username, password) {
+  return (
+    !username ||
+    !password ||
+    username.length < 4 ||
+    username.length > 20 ||
+    password.length < 8 ||
+    password.length > 20
+  );
+}
+
+module.exports = router;
